627ec43db0101bf1aab46188172fadc3a54e4f10
craig57836 <54261240+craig57836@users.noreply.github.com>
2019-08-19 15:22:22 +0100

    Binary search

---
+++ Binary search/Binary search/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.ListBox5 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 433)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(138, 24)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(441, 95)
+        Me.ListBox2.TabIndex = 1
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(138, 125)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(441, 95)
+        Me.ListBox3.TabIndex = 2
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Location = New System.Drawing.Point(138, 226)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(441, 95)
+        Me.ListBox4.TabIndex = 3
+        '
+        'ListBox5
+        '
+        Me.ListBox5.FormattingEnabled = True
+        Me.ListBox5.Location = New System.Drawing.Point(138, 327)
+        Me.ListBox5.Name = "ListBox5"
+        Me.ListBox5.Size = New System.Drawing.Size(441, 95)
+        Me.ListBox5.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(585, 93)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(182, 60)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(585, 300)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(182, 60)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Quit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox5)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents ListBox5 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Binary search/Binary search/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'clear listboxes
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+        ListBox3.Items.Clear()
+        ListBox4.Items.Clear()
+        ListBox5.Items.Clear()
+
+        'list sorting
+        'declare array of numbers and additional variables
+        Dim x As Integer
+        Dim temp As Integer
+        Dim outer As Integer
+        Dim inner As Integer
+        Dim numbers(15) As Integer
+
+        'generate random numbers between 1 and 100
+        Randomize()
+        For x = 0 To 15
+            numbers(x) = Int(Rnd() * 100) + 1
+        Next
+
+        'sort the numbers (Inesertion Sort)
+        For outer = 1 To 15
+            For inner = outer To 1 Step -1
+                If numbers(inner) < numbers(inner - 1) Then
+                    temp = numbers(inner)
+                    numbers(inner) = numbers(inner - 1)
+                    numbers(inner - 1) = temp
+
+                End If
+            Next inner
+        Next outer
+
+        'display numbers
+        For x = 1 To 15
+            ListBox1.Items.Add(numbers(x))
+        Next x
+
+        'binary search
+        Dim middle As Integer 'Middle position in array
+        Dim firstPosition As Integer 'first position in array
+        Dim lastPoisition As Integer 'last position in array
+        Dim searchKey As Integer 'item you are searching for
+        Dim passes As Integer 'counts number of times list has been halved
+        Dim found As Boolean
+
+        found = False
+        passes = 0
+        firstPosition = 0
+        lastPoisition = 15
+        searchKey = InputBox("Input the number you want to find")
+        ListBox2.Items.Add("The search key is" & searchKey)
+
+        Do
+            passes = passes + 1
+            middle = (firstPosition + lastPoisition) / 2 'find middle position
+            If numbers(middle) < searchKey Then
+                firstPosition = middle + 1  'discard left side of list
+            Else
+                lastPoisition = middle - 1  'discard right side of list
+            End If
+            If numbers(middle) = searchKey Then
+                found = True
+                ListBox3.Items.Add("Search Key " * searchKey & " Found at position " & middle)
+                ListBox4.Items.Add("Number of passes required " & passes)
+            End If
+        Loop Until found = True Or passes > 5
+        If passes > 5 Then
+            ListBox5.Items.Add("Search Key " & searchKey & " not found!")
+        End If
+    End Sub
+End Class
